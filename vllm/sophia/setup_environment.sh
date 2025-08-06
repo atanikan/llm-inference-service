f0d84d284d7f2dc5f92c3aaa0a19de4ff386ea25
Aditya Tanikanti <aditya.tanikanti@gmail.com>
2025-08-06 12:28:16 -0500

    Update setup_environment.sh

--- vllm/sophia/setup_environment.sh
+++ vllm/sophia/setup_environment.sh
@@ -15,15 +15,20 @@ module use /soft/modulefiles
 module load conda
 
 # Source the Conda initialization script directly
-conda activate vllm-sophia-env-072
+conda activate vllm-sophia-env-latest
 
 # Set environment variables
 export HF_DATASETS_CACHE='/eagle/projects/argonne_tpc/model_weights/'
 export HF_HOME='/eagle/projects/argonne_tpc/model_weights/'
+export HF_HUB_CACHE='/eagle/argonne_tpc/model_weights/hub'
 export RAY_TMPDIR='/tmp'
 export TMPDIR='/tmp'
-export NCCL_SOCKET_IFNAME='infinibond0'
-export VLLM_IMAGE_FETCH_TIMEOUT=120
 export HF_TOKEN='' # TODO: Add your HF token here
+export RAY_TMPDIR='/tmp'
+export NCCL_SOCKET_IFNAME='infinibond0'
+export OMP_NUM_THREADS=4
+export VLLM_IMAGE_FETCH_TIMEOUT=60
+export USE_FASTSAFETENSOR='true'
+ulimit -c unlimited
 
 echo "Environment setup complete."
